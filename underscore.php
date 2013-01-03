9acefeb6f97410af9672d3ce98369e75474d7044
Emon Tsukimiya <emonkak@gmail.com>
2013-01-04 02:36:28 +0900

    Fix autoloader

--- underscore.php
+++ underscore.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Underscore;
-
 spl_autoload_register(function($className) {
   if (strpos($className, 'Underscore\\') === 0) {
     $fileName = __DIR__
@@ -13,13 +11,3 @@ spl_autoload_register(function($className) {
     require($fileName);
   }
 });
-
-if (class_exists('Generator')) {
-  // Suppress syntax error on less than PHP 5.4.
-  eval('namespace Underscore { abstract class _ extends Underscore { use Generator; }}');
-} else {
-  abstract class _ extends Underscore {}
-}
-
-// __END__
-// vim: expandtab softtabstop=2 shiftwidth=2
