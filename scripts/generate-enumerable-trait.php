450935eeba77c5df31739bcfbe4ea956a86039c3
Shota Nozaki <emonkak@gmail.com>
2013-01-26 16:23:18 +0900

    generate-enumerable-trait.php: Remove unused code

--- scripts/generate-enumerable-trait.php
+++ scripts/generate-enumerable-trait.php
@@ -1,7 +1,7 @@
 <?php echo '<?php', "\n"; ?>
 namespace Understrike;
 trait Enumerable {
-<?php require __DIR__ . '/../vendor/autoload.php'; use Understrike\Strict as _; foreach (get_class_methods('Understrike\\Strict') as $m): ?>
+<?php require __DIR__ . '/../vendor/autoload.php'; foreach (get_class_methods('Understrike\\Strict') as $m): ?>
 public function <?php echo $m ?>(){$args=func_get_args();array_unshift($args,$this);return call_user_func_array('Understrike\\Strict::<?php echo $m ?>',$args);}
 <?php endforeach ?>
 public function lazy(){return Lazy::wrap($this);}
