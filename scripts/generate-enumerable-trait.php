0f39bcf955af330a5530f75a4cfe26e614839e6f
Shota Nozaki <emonkak@gmail.com>
2013-01-26 16:43:53 +0900

    Remove range() on Enumerable

--- scripts/generate-enumerable-trait.php
+++ scripts/generate-enumerable-trait.php
@@ -2,7 +2,9 @@
 namespace Understrike;
 trait Enumerable {
 <?php require __DIR__ . '/../vendor/autoload.php'; foreach (get_class_methods('Understrike\\Strict') as $m): ?>
+<?php if (!in_array($m, array('range'))): ?>
 public function <?php echo $m ?>(){$args=func_get_args();array_unshift($args,$this);return call_user_func_array('Understrike\\Strict::<?php echo $m ?>',$args);}
+<?php endif ?>
 <?php endforeach ?>
 public function lazy(){return Lazy::wrap($this);}
 }
