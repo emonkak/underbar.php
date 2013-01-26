b4db7c8df89a55db8e9508049fd39958ed56f6b0
Shota Nozaki <emonkak@gmail.com>
2013-01-26 16:45:44 +0900

    Refactor generate-enumerable-trait.php

--- scripts/generate-enumerable-trait.php
+++ scripts/generate-enumerable-trait.php
@@ -1,7 +1,7 @@
-<?php echo '<?php', "\n"; ?>
+<?php require __DIR__ . '/../vendor/autoload.php'; echo '<?php', "\n"; ?>
 namespace Understrike;
 trait Enumerable {
-<?php require __DIR__ . '/../vendor/autoload.php'; foreach (get_class_methods('Understrike\\Strict') as $m): ?>
+<?php foreach (get_class_methods('Understrike\\Strict') as $m): ?>
 <?php if (!in_array($m, array('range'))): ?>
 public function <?php echo $m ?>(){$args=func_get_args();array_unshift($args,$this);return call_user_func_array('Understrike\\Strict::<?php echo $m ?>',$args);}
 <?php endif ?>
