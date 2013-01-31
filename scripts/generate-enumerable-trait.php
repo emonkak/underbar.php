3dec4f989b744232fbdda56325859aefc1856cde
Shota Nozaki <emonkak@gmail.com>
2013-01-31 16:06:09 +0900

    generate-enumerable-trait.php: Fix don't generate alias method

--- scripts/generate-enumerable-trait.php
+++ scripts/generate-enumerable-trait.php
@@ -2,8 +2,8 @@
 namespace Underbar;
 trait Enumerable {
 <?php $r = new ReflectionClass('Underbar\\Strict'); foreach ($r->getMethods() as $m): ?>
-<?php if (preg_match('/\*\s+@category\s+(Collections|Arrays|Chaining)/', $m->getDocComment())): ?>
-public function <?php echo $m->getName() ?>(){$args=func_get_args();array_unshift($args,$this);return call_user_func_array('Underbar\\Strict::<?php echo $m->getName() ?>',$args);}
+<?php if ($m->isPublic() && preg_match('/\*\s+@category\s+(Collections|Arrays|Chaining)|^$/', $m->getDocComment())): ?>
+public function <?php echo $m->getName() ?>(){$args=func_get_args();array_unshift($args,$this);return call_user_func_array('<?php echo $r->getName().'::'.$m->getName() ?>',$args);}
 <?php endif ?>
 <?php endforeach ?>
 public function lazy(){return Lazy::chain($this);}
