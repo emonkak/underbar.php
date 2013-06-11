a7fc7f08e60fd01151aded83ba5c2aadd8e3e61e
Shota Nozaki <emonkak@gmail.com>
2013-06-12 00:44:01 +0900

    ZipIterator: Remove the index variable

--- src/Underbar/Iterator/ZipIterator.php
+++ src/Underbar/Iterator/ZipIterator.php
@@ -4,8 +4,6 @@ namespace Underbar\Iterator;
 
 class ZipIterator extends \MultipleIterator
 {
-    private $index;
-
     public function __construct()
     {
         parent::__construct(self::MIT_NEED_ALL | self::MIT_KEYS_NUMERIC);
@@ -13,18 +11,5 @@ class ZipIterator extends \MultipleIterator
 
     public function key()
     {
-        return $this->index;
-    }
-
-    public function rewind()
-    {
-        parent::rewind();
-        $this->index = 0;
-    }
-
-    public function next()
-    {
-        parent::next();
-        $this->index++;
     }
 }
